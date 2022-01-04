e08d9d962bc5407842f30781485cf4fb423df62c
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-04 22:02:40 +0700

    A better practice is to define styles

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { CSSProperties } from "react";
 
 function App() {
+  // A better practice is to define styles in a separate constant:
+  const buttonStyles: CSSProperties = {
+    backgroundColor: "#5aac44",
+    borderRadius: "3px",
+    border: "none",
+    boxShadow: "none",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
