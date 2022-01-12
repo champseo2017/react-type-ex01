33f32641e381399e6ffc5679924b55e317c6146f
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-12 21:57:02 +0700

    Create Input Form. Using Events

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,17 +1,19 @@
-import React, { CSSProperties } from "react";
-
+import { Column } from "./Column";
+import { Card } from "./Card";
+import { AppContainer } from "./styles";
 function App() {
-  // A better practice is to define styles in a separate constant:
-  const buttonStyles: CSSProperties = {
-    backgroundColor: "#5aac44",
-    borderRadius: "3px",
-    border: "none",
-    boxShadow: "none",
-  };
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Being to use static typing" />
+      </Column>
+    </AppContainer>
   );
 }
 
